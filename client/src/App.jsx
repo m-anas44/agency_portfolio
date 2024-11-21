9f8b367817a6d2aa45733688c8fe7ca7cf58f203
Muhammad Mudasir <167820839+muhammadmudasirawan@users.noreply.github.com>
2024-11-21 21:39:35 +0500

    mudasir

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Welcome to the agency </h1>
-      <p>I am anas</p>
-      <p>you are not zohaib. its me</p>
+      <p>hello i am mudasir</p>
     </>
   );
 }
