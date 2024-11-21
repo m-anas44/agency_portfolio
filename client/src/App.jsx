8af6ce3772b6246bf410ce8360010fb74ab26b15
zohaib sadiq <abdullahjavaid899@gmail.com>
2024-11-21 22:59:34 +0500

    install tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to the agency </h1>
-      <p>i am pulling 2 time</p>
+      <h1 className="bg-red-500">Welcome to the agency </h1>
+      <p className="bg-red-300">i am pulling 2 time</p>
     </>
   );
 }
