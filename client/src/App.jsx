e626c702a17b96103272443e39ce9b47fb029723
Muhammad Mudasir <167820839+muhammadmudasirawan@users.noreply.github.com>
2024-11-25 21:37:42 +0500

    about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,13 @@
 
-import "./App.css";
-import OverMission from "./assets/About/OverMission";
 
-import About from "./Pages/About/About";
-import AboutUs from "./Pages/About/AboutUs";
-import CallToAction from "./Pages/About/CallToAction";
-import FeatureCards from "./Pages/About/FeatureCards";
-import OurMission from "./Pages/About/OurMission";
-import OurVision from "./Pages/About/OurVision";
-import TeamLead from "./Pages/About/TeamLeads";
-import ValuesSection from "./Pages/About/ValuseSection";
 
 const App = () => {
   return (
     <>
       <h1 className="poppins-bold">React App</h1>
     </>
-=========
-    <div>
-      <About />
-      <OverMission />
-    </div>
->>>>>>>>> Temporary merge branch 2
   );
 };
-CallToAction;
+
 
 export default App;
